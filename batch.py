c5e01f8647382a6766181662539d74e7f153a1a1
Wee Keat Chin <weekeat@visualdensity.com>
2013-09-12 03:36:16 +0200

    A little tweak

--- batch.py
+++ batch.py
@@ -3,9 +3,10 @@
 from subprocess import call
 from random import *
 
-logTypes = ['application.info', 'application.error', 'application.warning', 'system.error', 'system.warning', 'event.hit', 'event.view', 'event.share']
+#logTypes = ['application.info', 'application.error', 'application.warning', 'system.error', 'system.warning', 'event.hit', 'event.view', 'event.share']
+logTypes = ['application.info', 'application.error', 'system.error', 'system.info']
 
-for i in range (0,100):
+for i in range (0,1000):
     rand_seed = randrange( randrange(1,123), randrange(888,65777) )
     #print i+1, ": " , rand_seed
     #print choice(logTypes)
