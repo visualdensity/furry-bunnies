301a38cf210e026372c981a1652734351df43be9
Wee Keat Chin <weekeat@visualdensity.com>
2014-01-21 15:54:54 +1100

    clean up and larger batching

--- batch.py
+++ batch.py
@@ -6,10 +6,6 @@ from random import *
 #logTypes = ['application.info', 'application.error', 'application.warning', 'system.error', 'system.warning', 'event.hit', 'event.view', 'event.share']
 logTypes = ['application.info', 'application.error', 'system.error', 'system.info']
 
-for i in range (0,10000):
+for i in range (0,20000):
     rand_seed = randrange( randrange(1,123), randrange(888,65777) )
-    #print i+1, ": " , rand_seed
-    #print choice(logTypes)
-    #print './publisher.topic.php "%s" "Some seed - %d"' % (choice(logTypes), rand_seed)
     call('./publisher.topic.php "%s" "Some seed - %d"' % (choice(logTypes), rand_seed), shell=True)
-
